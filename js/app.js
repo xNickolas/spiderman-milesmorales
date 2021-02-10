8fb734fc74a2a7faa931380c85347235c8c0ad0d
xNickolas <nickolsctt@gmail.com>
2021-02-10 04:48:24 -0300

    small changes

--- js/app.js
+++ js/app.js
@@ -4,42 +4,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
   tl.fromTo('.bg-loader', 1, 
   {width: '100%'},
-  {width: '0%', delay: 5, ease: Expo.easeInOut})
+  {width: '0%', delay: 4, ease: Expo.easeInOut})
 
   tl.fromTo('.bg-video', 2, 
   {width: '0%', opacity: 0},
   {width: '100%', opacity: 1, ease: Expo.easeInOut}, '-=1')
 
-  tl.fromTo('.logo', 0.7, 
+  tl.fromTo('.logo', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.nav-list', 0.7, 
+  tl.fromTo('.nav-list', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.nav-social', 0.7, 
+  tl.fromTo('.nav-social', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.item-1', 0.7, 
+  tl.fromTo('.item-1', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.item-2', 0.7, 
+  tl.fromTo('.item-2', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.item-3', 0.7, 
+  tl.fromTo('.item-3', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.item-4', 0.7, 
+  tl.fromTo('.item-4', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
-  tl.fromTo('.item-5', 0.7, 
+  tl.fromTo('.item-5', 0.5, 
   {y: -50, opacity: 0},
-  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+  {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.2')
 
 })
